69758bd488180454cba8f8829892da006d5e2da9
Arthur Negrão <arthurnegrao10s@gmail.com>
2026-03-31 11:15:39 -0300

    feat: Commit inicial
    Estruturação inicial dos diretórios

---
+++ Rota-Do-Verde/Rota-Do-Verde/scenes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rota-Do-Verde
+//
+//  Created by Turma01-28 on 31/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rota-Do-Verde/Rota-Do-Verde/scenes/Rota_Do_VerdeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rota_Do_VerdeApp.swift
+//  Rota-Do-Verde
+//
+//  Created by Turma01-28 on 31/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Rota_Do_VerdeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
