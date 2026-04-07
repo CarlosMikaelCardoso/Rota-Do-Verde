0e1bf67116a28700e6b1d12f13372367596347f0
Esdras Silva <esdrasdev0@gmail.com>
2026-04-07 15:21:56 -0300

    feat: colocando as novas rotas no model e service api

---
+++ Rota-Do-Verde/Rota-Do-Verde/model/CriarPontoRequest.swift
@@ -0,0 +1,8 @@
+//
+//  CriarPontoRequest.swift
+//  Rota-Do-Verde
+//
+//  Created by Turma01-1 on 07/04/26.
+//
+
+import Foundation
---
+++ Rota-Do-Verde/Rota-Do-Verde/model/EditarPontoRequest.swift
@@ -0,0 +1,8 @@
+//
+//  EditarPontoRequest.swift
+//  Rota-Do-Verde
+//
+//  Created by Turma01-1 on 07/04/26.
+//
+
+import Foundation
