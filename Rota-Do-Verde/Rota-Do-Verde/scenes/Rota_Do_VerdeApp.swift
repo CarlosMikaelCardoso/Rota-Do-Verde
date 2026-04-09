326cbae821e910651af9911d76f5deeb9127a8a2
Carlos Mikael Cardoso Da Costa <mikael.cardoso.costa13@gmail.com>
2026-04-09 15:41:52 -0300

    Adicionado uma pagina de configuração para mudar o tema do APP

--- Rota-Do-Verde/Rota-Do-Verde/scenes/Rota_Do_VerdeApp.swift
+++ Rota-Do-Verde/Rota-Do-Verde/scenes/Rota_Do_VerdeApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 @main
 struct Rota_Do_VerdeApp: App {
+    // 1. Lemos a variável global salva no AppStorage
+    @AppStorage("temaApp") private var temaAtual: TemaApp = .sistema
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                // 2. Aplicamos o tema diretamente na raiz, forçando todo o app a mudar
+                .preferredColorScheme(temaAtual.colorScheme)
         }
     }
 }
